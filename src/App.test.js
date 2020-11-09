fcdffd2a41f160b3190042c6b05d99c6d35819b1
Ray Maldonado <vivachihuahua2004@gmail.com>
2020-11-09 16:35:27 -0600

    Ejecting option for changes json files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders withou crashing', () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper).toBeTruthy();
+
+});
+
+test("renders without error", () => {
+  const wrapper = shallow(<App ></App>);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test("renders counter display", () => {
+
+});
+
+test("counter starts at 0", () => {
+
+});
+
+test("clicking on button increments counter display", () => {
+
 });
