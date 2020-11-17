e8b693b69570da9492a899a00cb63892ccd71c40
Ray Maldonado <vivachihuahua2004@gmail.com>
2020-11-17 17:04:06 -0600

    Start tests for GuessedWords

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { shallow } from 'enzyme';
 
 import Congrats from './Congrats';
-import { findByTestAttr, checkProps } from './testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
 
 
